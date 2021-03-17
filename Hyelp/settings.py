d27eb03fabd9fa273cfbf073aa52627cc3def449
AbdullahNoori <abdullah.noori@students.makeschool.com>
2021-03-16 19:06:29 -0700

    update

--- Hyelp/settings.py
+++ Hyelp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5mdvx%ks(8fnb#%av%4sde$gstv1_sr_=7pkuvex3%b2wh=($c'
+SECRET_KEY = '5mdvx%ks(8fnb#%av%4sde$gstv1_sr_=7pkuvex3%b2wh=($cgyuvbh@ijnlkm$jop90u8y32qwe$$23erwdfv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'Hyde'
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
