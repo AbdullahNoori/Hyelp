1013ee954f61de572ed3678fda2bc5d823160c7d
vladyslavnUA <vladyslavn@yahoo.com>
2021-03-18 17:30:10 -0400

    updated app

--- app/views.py
+++ app/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 from .yelp import YelpAPI
-# Create your views here.
 from django.http import HttpResponse
-
-
-
 
 def index(request):
     yelp = YelpAPI()
     print(yelp.search("barber", "nyc"))
-    
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'app/home-v2.html')
+    # return HttpResponse("Hello, world. You're at the polls index.")
